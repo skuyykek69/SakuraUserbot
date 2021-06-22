d0551d151e8a7210fc1d7ad1c0ade16ca6b6ce39
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-22 12:03:34 +0700

    Update banuser.py

--- assistant/pmbot/banuser.py
+++ assistant/pmbot/banuser.py
@@ -15,17 +15,17 @@ async def banhammer(event):
         return
     x = await event.get_reply_message()
     if x is None:
-        return await event.edit("Please reply to someone to ban him.")
+        return await event.edit("balas ke pesan untuk melakukan banned.")
     target = get_who(x.id)
     if not is_blacklisted(target):
         blacklist_user(target)
-        await asst.send_message(event.chat_id, f"#BAN\nUser - {target}")
+        await asst.send_message(event.chat_id, f"#BAN\npengguna - {target}")
         await asst.send_message(
             target,
-            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
+            "`woi asu! lu udah dibanned yah.`\n**jadi, pesan yang lu kirim ke sini gak bakal terkirim lagi.**",
         )
     else:
-        return await asst.send_message(event.chat_id, f"User is already banned!")
+        return await asst.send_message(event.chat_id, f"manusia tolol telah dibanned!")
 
 
 @asst_cmd("unban")
@@ -35,11 +35,11 @@ async def banhammer(event):
         return
     x = await event.get_reply_message()
     if x is None:
-        return await event.edit("Please reply to someone to ban him.")
+        return await event.edit("balas ke pesan untuk melakukan unbanned.")
     target = get_who(x.id)
     if is_blacklisted(target):
         rem_blacklist(target)
-        await asst.send_message(event.chat_id, f"#UNBAN\nUser - {target}")
-        await asst.send_message(target, "`Congrats! You have been unbanned.`")
+        await asst.send_message(event.chat_id, f"#UNBAN\npengguna - {target}")
+        await asst.send_message(target, "`selamat ya! lu udah di unbanned sama tuan gua.`")
     else:
-        return await asst.send_message(event.chat_id, f"User was never banned!")
+        return await asst.send_message(event.chat_id, f"pengguna sudah di unbanned!")
