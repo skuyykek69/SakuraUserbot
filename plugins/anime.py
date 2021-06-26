aec8449a4249cd79250401463c17f6c2216a9dcd
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-26 17:10:28 +0700

    Update anime.py

--- plugins/anime.py
+++ plugins/anime.py
@@ -6,16 +6,16 @@
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 """
-✘ Commands Available -
+📚 Commands Available : Berikut fungsi yg tersedia.
 
 • `{i}airing`
-   Get details about currently airing anime.
+   Dapatkan detail tentang anime yang sedang ditayangkan.
 
 • `{i}anime <anime name>`
-   Get anime details from anilist.
+   Dapatkan detail anime dari anilist.
 
 • `{i}character <character name>`
-   Fetch anime character details.
+   Dapatkan detail karakter anime.
 """
 
 from os import remove
@@ -44,7 +44,7 @@ async def anilist(event):
     name = event.pattern_match.group(1)
     x = await eor(event, get_string("com_1"))
     if not name:
-        return await eod(x, "`Enter a anime name!`", time=5)
+        return await eod(x, "`berikan nama anime!`", time=5)
     banner, title, year, episodes, info = get_anime_src_res(name)
     msg = f"**{title}**\n{year} | {episodes} Episodes.\n\n{info}"
     await event.reply(msg, file=banner, link_preview=True)
@@ -56,12 +56,12 @@ async def anime_char_search(event):
     xx = await eor(event, get_string("com_1"))
     char_name = event.pattern_match.group(1)
     if not char_name:
-        await eod(xx, "`Enter the name of a character too please!`", time=5)
+        await eod(xx, "`berikan nama dari sang karakter juga!`", time=5)
     jikan = jikanpy.jikan.Jikan()
     try:
         s = jikan.search("character", char_name)
     except jikanpy.exceptions.APIException:
-        return await eod(xx, "`Couldn't find character!`", time=5)
+        return await eod(xx, "`tidak dapat menemukan karakter!`", time=5)
     a = s["results"][0]["mal_id"]
     char_json = jikan.character(a)
     pic = char_json["image_url"]
@@ -72,8 +72,8 @@ async def anime_char_search(event):
         msg += "\n"
     if char_json["nicknames"]:
         nicknames_string = ", ".join(char_json["nicknames"])
-        msg += f"\n**Nicknames** : `{nicknames_string}`\n"
+        msg += f"\n**nicknames** : `{nicknames_string}`\n"
     about = char_json["about"].split("\n", 1)[0].strip().replace("\n", "")
-    msg += f"\n**About**: __{about}__"
+    msg += f"\n**about**: __{about}__"
     await event.reply(msg, file=pic, force_document=False)
     await xx.delete()
